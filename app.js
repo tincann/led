9a7992f829816856e036d67947fa2d3a7c10e401
tincann <saabnl@gmail.com>
2015-12-29 11:10:49 +0100

    System works

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var sensors = require('./lib/sensors.js');
 var actuators = require('./lib/actuators.js');
 var System = require('./lib/system.js');
 
-var pirSensor = new sensors.PIR();
-var led = new actuator.LED();
-var system = new System({sensor: pirSensor, actuator: led});
+// var pirSensor = new sensors.PIR();
+// var led = new actuator.LED();
+var pirSensor = new sensors.FakePIR();
+var led = new actuators.FakeLED();
+
+var system = new System(pirSensor, led);
 
 system.run();
