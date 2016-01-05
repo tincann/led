14a99e30222031a340e318a719a57080d1501c86
tincann <saabnl@gmail.com>
2016-01-05 21:55:51 +0100

    Fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var sensors = require('./lib/sensors.js');
 var actuators = require('./lib/actuators.js');
 var System = require('./lib/system.js');
 
-var pirSensor = new sensors.PIR();
+var pirSensor = new sensors.FakePIR(1000);
 var led = new actuators.LED(1);
 
 var system = new System(pirSensor, led);
