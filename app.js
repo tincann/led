ecfede017346ae000ed2996168c0dde3311cddb9
tincann <saabnl@gmail.com>
2016-01-05 22:24:23 +0100

    Led driver

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var sensors = require('./lib/sensors.js');
 var actuators = require('./lib/actuators.js');
+var LedDriver = require('./led-driver.js');
 var System = require('./lib/system.js');
 
 var pirSensor = new sensors.FakePIR(1000);
-var led = new actuators.LED(1);
 
-var system = new System(pirSensor, led);
+
+
+var led = new actuators.LED(1);
+var driver = new LedDriver(led);
+var system = new System(pirSensor, driver);
 
 system.run();
