dd62f496aca26e8c7db021848573c0eccb68f550
tincann <saabnl@gmail.com>
2016-01-05 21:28:58 +0100

    Added ws128x library

--- local.js
+++ local.js
@@ -2,8 +2,10 @@ var sensors = require('./lib/sensors.js');
 var actuators = require('./lib/actuators.js');
 var System = require('./lib/system.js');
 
-var pirSensor = new sensors.PIR();
-var led = new actuator.LED(1);
+// var pirSensor = new sensors.PIR();
+// var led = new actuator.LED();
+var pirSensor = new sensors.FakePIR();
+var led = new actuators.FakeLED();
 
 var system = new System(pirSensor, led);
 
