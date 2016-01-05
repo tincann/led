d4cf446a9aeea91ade826a96fb4ae17b764f1bf7
tincann <saabnl@gmail.com>
2016-01-05 21:49:34 +0100

    Fixed bug

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var actuators = require('./lib/actuators.js');
 var System = require('./lib/system.js');
 
 var pirSensor = new sensors.PIR();
-var led = new actuator.LED(1);
+var led = new actuators.LED(1);
 
 var system = new System(pirSensor, led);
 
